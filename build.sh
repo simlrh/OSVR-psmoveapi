dff42763d6fc46fe77ddbf75ca0821fa317ec961
Steve Le Roy Harris <steve@nourish.je>
2017-04-10 20:33:07 +0100

    Consolidate psmove patches

--- build.sh
+++ build.sh
@@ -2,10 +2,7 @@
 
 git submodule update --init --recursive
 cd vendor/psmoveapi
-git update-index --assume-unchanged src/tracker/platform/camera_control_linux.c
-git apply -vvv ../../psmoveapi-patch
-git apply -vvv ../../add-libv4l2-module.patch
-git apply -vvv ../../fix-opencv-headers.patch
+git apply -vvv ../../psmoveapi.patch
 cd ../..
 mkdir -p build
 cd build
